6c7f44402cbbf47cae59a4d6f6333c9a32100d47
JavierCalquin <javi.calquin@duocuc.cl>
2026-06-03 17:52:07 -0400

    Update EmpleadoRepository.java

--- hostal cordillera/src/main/java/com/example/hostadmin/repository/EmpleadoRepository.java
+++ hostal cordillera/src/main/java/com/example/hostadmin/repository/EmpleadoRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.hostadmin.model.Empleado;
 // id es rut, por eso es String
+@Repository
 public interface EmpleadoRepository extends JpaRepository<Empleado, String> {
 
 }
