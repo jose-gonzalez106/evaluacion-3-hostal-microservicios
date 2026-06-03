bff415e1da94b588eae0bc81af9f27ce05f618b5
JavierCalquin <javi.calquin@duocuc.cl>
2026-06-03 17:52:45 -0400

    Update HostalRepository.java

--- hostal cordillera/src/main/java/com/example/hostadmin/repository/HostalRepository.java
+++ hostal cordillera/src/main/java/com/example/hostadmin/repository/HostalRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.hostadmin.model.Hostal;
 
+@Repository
 public interface HostalRepository extends JpaRepository<Hostal, Long>  {
     boolean existsByRutEmpresa(String rutEmpresa);
 
