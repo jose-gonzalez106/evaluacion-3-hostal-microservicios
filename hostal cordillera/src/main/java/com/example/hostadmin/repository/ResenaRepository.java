4d296be39b1f80e4617b7fb39ed6dd565629101f
JavierCalquin <javi.calquin@duocuc.cl>
2026-06-03 17:53:57 -0400

    Update ResenaRepository.java

--- hostal cordillera/src/main/java/com/example/hostadmin/repository/ResenaRepository.java
+++ hostal cordillera/src/main/java/com/example/hostadmin/repository/ResenaRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.hostadmin.model.Resena;
 
+@Repository
 public interface ResenaRepository extends JpaRepository<Resena, Long> {
     List<Resena> findByHuespedRun(String run);
 }
