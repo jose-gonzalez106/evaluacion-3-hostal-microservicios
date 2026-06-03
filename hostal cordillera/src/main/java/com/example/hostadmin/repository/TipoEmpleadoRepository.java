cd445552b0327b7a9384956b07e24f43f1a3b3b5
JavierCalquin <javi.calquin@duocuc.cl>
2026-06-03 17:54:23 -0400

    Update TipoEmpleadoRepository.java

--- hostal cordillera/src/main/java/com/example/hostadmin/repository/TipoEmpleadoRepository.java
+++ hostal cordillera/src/main/java/com/example/hostadmin/repository/TipoEmpleadoRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.hostadmin.model.TipoEmpleado;
 
+@Repository
 public interface TipoEmpleadoRepository extends JpaRepository<TipoEmpleado, Long> {
     boolean existsByCategoria(String categoria);
 }
