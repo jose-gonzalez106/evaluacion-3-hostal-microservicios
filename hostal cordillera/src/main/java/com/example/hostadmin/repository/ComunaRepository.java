51fd1af65df7396eb7f50c5fa47e4fe0b3986b23
JavierCalquin <javi.calquin@duocuc.cl>
2026-06-03 17:51:21 -0400

    Add @Repository annotation to ComunaRepository

--- hostal cordillera/src/main/java/com/example/hostadmin/repository/ComunaRepository.java
+++ hostal cordillera/src/main/java/com/example/hostadmin/repository/ComunaRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.hostadmin.model.Comuna;
 
+@Repository
 public interface ComunaRepository extends JpaRepository<Comuna, Long>{
 
 }
