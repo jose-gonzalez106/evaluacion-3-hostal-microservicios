cd17856dc56c75452e215c766356c1fe540e57dd
JavierCalquin <javi.calquin@duocuc.cl>
2026-06-03 17:52:58 -0400

    Update HuespedRepository.java

--- hostal cordillera/src/main/java/com/example/hostadmin/repository/HuespedRepository.java
+++ hostal cordillera/src/main/java/com/example/hostadmin/repository/HuespedRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.hostadmin.model.Huesped;
 //mismo caso de empleado, el id es rut, por eso es String
+@Repository
 public interface HuespedRepository extends JpaRepository<Huesped, String> {
     boolean existsByCorreo(String correo);
 }
