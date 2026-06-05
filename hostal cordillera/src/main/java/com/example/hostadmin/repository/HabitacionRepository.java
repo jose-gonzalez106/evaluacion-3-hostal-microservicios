7fc99ee22d261437b15fd4bc5c101d142c707b46
JavierCalquin <javi.calquin@duocuc.cl>
2026-06-05 17:00:17 -0400

    Update HabitacionRepository.java

--- hostal cordillera/src/main/java/com/example/hostadmin/repository/HabitacionRepository.java
+++ hostal cordillera/src/main/java/com/example/hostadmin/repository/HabitacionRepository.java
@@ -2,7 +2,7 @@ package com.example.hostadmin.repository;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import com.example.hostadmin.model.Habitacion;
 //usan número de habitación como id, por eso es Integer
 @Repository
