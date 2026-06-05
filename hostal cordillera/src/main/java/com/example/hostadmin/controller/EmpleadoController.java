150614a53b17de2386bf9817ccdb7be4f7110825
JavierCalquin <javi.calquin@duocuc.cl>
2026-06-05 17:03:58 -0400

    Update EmpleadoController.java

--- hostal cordillera/src/main/java/com/example/hostadmin/controller/EmpleadoController.java
+++ hostal cordillera/src/main/java/com/example/hostadmin/controller/EmpleadoController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
 import com.example.hostadmin.DTO.EmpleadoDTO;
 import com.example.hostadmin.model.Empleado;
