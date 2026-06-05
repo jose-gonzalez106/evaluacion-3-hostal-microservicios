07d11924cfab153a54f0cee6df59247a33f7c0b9
JavierCalquin <javi.calquin@duocuc.cl>
2026-06-05 17:01:52 -0400

    Update ReservaRepository.java

--- hostal cordillera/src/main/java/com/example/hostadmin/repository/ReservaRepository.java
+++ hostal cordillera/src/main/java/com/example/hostadmin/repository/ReservaRepository.java
@@ -3,7 +3,7 @@ package com.example.hostadmin.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import com.example.hostadmin.model.Reserva;
 
 @Repository
