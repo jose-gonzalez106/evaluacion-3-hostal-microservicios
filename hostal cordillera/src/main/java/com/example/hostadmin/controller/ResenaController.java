2bdb54fecda10a03253513fee7db41bb47a46a71
JavierCalquin <javi.calquin@duocuc.cl>
2026-06-05 17:05:05 -0400

    Update ResenaController.java

--- hostal cordillera/src/main/java/com/example/hostadmin/controller/ResenaController.java
+++ hostal cordillera/src/main/java/com/example/hostadmin/controller/ResenaController.java
@@ -14,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import com.example.hostadmin.DTO.ResenniaDTO;
 import com.example.hostadmin.model.Resena;
 import com.example.hostadmin.service.ResenaService;
+import org.springframework.stereotype.Controller;
 import jakarta.validation.Valid;
 
 @Controller
