48f491e27d809eda507e9b79164a9052491fba3d
jose-gonzalez106 <jo.gonzalez7@duocuc.cl>
2026-06-19 17:28:12 -0400

    coreccion de WebClientConfig

--- huespedes/src/main/java/com/huesped/huespedes/config/WebClientConfig.java
+++ huespedes/src/main/java/com/huesped/huespedes/config/WebClientConfig.java
@@ -1,14 +1,25 @@
 package com.huesped.huespedes.config;
 
+import java.util.List;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class WebClientConfig {
 
     @Bean
+    @LoadBalanced
     public WebClient.Builder webClientBuilder() {
         return WebClient.builder();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .servers(List.of(new Server().url("/").description("Microservicio Huespedes")));
     }
 }
