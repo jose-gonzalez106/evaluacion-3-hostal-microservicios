77fc80bed58e466c39a74d9c1452a1fb1bb7cff8
JavierCalquin <javi.calquin@duocuc.cl>
2026-06-22 15:18:36 -0400

    Update WebClientConfig.java

--- huespedes/src/main/java/com/huesped/huespedes/config/WebClientConfig.java
+++ huespedes/src/main/java/com/huesped/huespedes/config/WebClientConfig.java
@@ -1,12 +1,9 @@
 package com.huesped.huespedes.config;
 
-import java.util.List;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class WebClientConfig {
@@ -15,11 +12,5 @@ public class WebClientConfig {
     @LoadBalanced
     public WebClient.Builder webClientBuilder() {
         return WebClient.builder();
-    }
-
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .servers(List.of(new Server().url("/").description("Microservicio Huespedes")));
     }
 }
