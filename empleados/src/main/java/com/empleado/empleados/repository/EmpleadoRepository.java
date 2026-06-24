0c6da180315cd753bf66d252d38842b487da151c
JavierCalquin <javi.calquin@duocuc.cl>
2026-06-24 17:48:44 -0400

    Fix merge conflicts in EmpleadoRepository
    Resolved merge conflicts and updated imports for EmpleadoRepository.

--- empleados/src/main/java/com/empleado/empleados/repository/EmpleadoRepository.java
+++ empleados/src/main/java/com/empleado/empleados/repository/EmpleadoRepository.java
@@ -1,14 +1,10 @@
 package com.empleado.empleados.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-<<<<<<< HEAD:empleados/src/main/java/com/empleado/empleados/repository/EmpleadoRepository.java
+import org.springframework.stereotype.Repository;
 
 import com.empleado.empleados.model.Empleado;
-=======
-import org.springframework.stereotype.Repository;
-import com.example.hostadmin.model.Empleado;
->>>>>>> origin/javier:hostal cordillera/src/main/java/com/example/hostadmin/repository/EmpleadoRepository.java
-// id es rut, por eso es String
+
 @Repository
 public interface EmpleadoRepository extends JpaRepository<Empleado, String> {
 
