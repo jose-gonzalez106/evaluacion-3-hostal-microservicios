19d02c9624e3e77789e6ba969102d03bf64abef3
jose-gonzalez106 <jo.gonzalez7@duocuc.cl>
2026-06-25 23:36:56 -0400

    correccion de duplicado en el nombre

--- hostal cordillera/src/test/java/com/example/hostadmin/service/ReservaServiceTest.java
+++ hostal cordillera/src/test/java/com/example/hostadmin/service/ReservaServiceTest.java
@@ -1,4 +1,4 @@
-package com.example.hostadmin.service.service;
+package com.example.hostadmin.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
