07a9f161a722d789c0267db7531b30132c3af085
jose-gonzalez106 <jo.gonzalez7@duocuc.cl>
2026-06-25 22:27:48 -0400

    cambios en start.bat para iniciar todos los microservicios y se da puerto a empleados

--- start.bat
+++ start.bat
@@ -25,8 +25,17 @@ echo Iniciando Gateway...
 cd ..\gateway
 start cmd /k "mvnw spring-boot:run"
 
+echo Iniciando Empleados...
+cd ..\empleados
+start cmd /k "mvnw spring-boot:run"
+
+echo Esperando 10 segundos...
+timeout /t 10 /nobreak > nul
+
 echo Ecosistema lanzado.
 echo Eureka dashboard: http://localhost:8761
 echo Huespedes: http://localhost:8081
-echo Gateway: http://localhost:8080
+echo Hostal Cordillera: http://localhost:8082
+echo Empleados: http://localhost:8083
+echo Gateway: http://localhost:8090
 pause
